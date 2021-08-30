5e7f99d4b8af45017ac4b28c9295689e93997988
MizukiSaitou <r2.f4g.gg9y.rj@gmail.com>
2021-08-30 22:37:15 +0900

    Add chapter2

---
+++ chapter2/New_line.java
@@ -0,0 +1,10 @@
+public class New_line{
+  public static void main(String[] args){
+    String name = "すがわら";
+
+    //printにすると改行しない
+    System.out.print("私の名前は");
+    System.out.print(name);
+    System.out.print("です。");
+  }
+}
