4e33780a9eae18fbddf447e31de61355b4e7af22
MizukiSaitou <r2.f4g.gg9y.rj@gmail.com>
2021-09-13 00:05:20 +0900

    review

---
+++ chapter6/Main.java
@@ -0,0 +1,10 @@
+import comment_r1.Zenhan;
+
+public class Main{
+  public static void main(String[] args) throws Exception{
+    Zenhan.doWarusa();
+    Zenhan.doTogame();
+    comment_r1.Kouhan.callDeae();
+    comment_r1.Kouhan.showMondokoro();
+  }
+}
